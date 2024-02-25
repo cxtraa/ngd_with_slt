4139e262fc518d0f1955eb07aaa5b4e243942674
cxtraa <moosasaghir10@gmail.com>
2024-02-25 18:30:25 +0000

    edited mnist_optimisers.py

--- experiments/mnist/mnist_vary_HN.bat
+++ experiments/mnist/mnist_vary_HN.bat
@@ -2,8 +2,8 @@
 @echo off
 
 REM Define the sets of hidden nodes to iterate over
-SET hidden_nodes=2 4 8 16 32 64 128 256 512 1024 2048 4096 8192 16384
-SET hidden_layers=2
+SET hidden_nodes=2 4 8 16 32 64 128 256 512 1024 2048
+SET hidden_layers=4
 
 REM Loop over each hidden node value
 FOR %%h IN (%hidden_nodes%) DO (
