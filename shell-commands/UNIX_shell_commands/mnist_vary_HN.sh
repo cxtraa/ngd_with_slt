e0e731df1ec53a76da8377c9383b29d088a00e2d
liuzihe02 <liuzihenanhua@gmail.com>
2024-03-01 22:11:34 +0000

    modifying optimisers

--- shell-commands/UNIX_shell_commands/mnist_vary_HN.sh
+++
@@ -1,8 +0,0 @@
-hidden_nodes=(2 4 8)
-hidden_layers=2
-
-for HN in "${hidden_nodes[@]}"
-    do 
-        python train.py --model LM --LMHN $HN --LMHL $hidden_layers --num_workers 12 --batch_size 128 --num_epochs 5
-        #echo $HL
-    done
