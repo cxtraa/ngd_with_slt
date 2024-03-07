e81694c3e3aa72a92ca09e70344dfa76cfae162d
liuzihe02 <liuzihenanhua@gmail.com>
2024-03-07 00:33:31 +0000

    changing CNN model

--- shell-commands/UNIX_shell_commands/train_LM_mnist_vary_optimizers.sh
+++ shell-commands/UNIX_shell_commands/train_LM_mnist_vary_optimizers.sh
@@ -4,11 +4,11 @@ for optim in "${optimizers[@]}"
 do 
     #specify different lr for sgd or ngd
     if [ "$optim" = "sgd" ]; then
-        lr=1e-2
+        lr=1
     elif [ "$optim" = "ngd" ]; then
         lr=1e-2
     fi
 
-    python train.py --model CM --optimiser $optim --lr $lr --num_workers 32 --batch_size 4086 --num_epochs 30 --CMKS 4 --CMHL 8
+    python train.py --model CM --optimiser $optim --lr $lr --num_workers 32 --batch_size 512 --num_epochs 30 --CMKS 4 --CMHL 4
 done
 
