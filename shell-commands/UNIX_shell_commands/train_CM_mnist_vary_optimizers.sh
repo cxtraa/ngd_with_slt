59013a91981cec777ead7ea2db1a732e0f1fa5bb
liuzihe02 <liuzihenanhua@gmail.com>
2024-03-07 00:21:10 +0000

    testing old NGD module

--- shell-commands/UNIX_shell_commands/train_CM_mnist_vary_optimizers.sh
+++ shell-commands/UNIX_shell_commands/train_CM_mnist_vary_optimizers.sh
@@ -1,7 +1,13 @@
-
 optimizers=(sgd ngd)
 
 for optim in "${optimizers[@]}"
-    do 
-        python train.py --model CM --optimiser $optim  --num_workers 12 --batch_size 128 --num_epochs 5 --CMKS 5 --CMHL 10
-    done
+do 
+    #specify different lr for sgd or ngd
+    if [ "$optim" = "sgd" ]; then
+        lr=1e-3
+    elif [ "$optim" = "ngd" ]; then
+        lr=1e-2
+    fi
+
+    python train.py --model CM --optimiser $optim --lr $lr --num_workers 12 --batch_size 1024 --num_epochs 5 --CMKS 5 --CMHL 10
+done
